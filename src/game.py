5a84dd563a3f2dacc4f6e33ce67d73c99f56ace9
1clone47 <andy.1clone47@gmail.com>
2022-07-13 22:02:31 +0200

    added command to login into admin

--- src/game.py
+++ src/game.py
@@ -1,256 +1,135 @@
+import sys
 import random
 
-field_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+PASSWORD = "admin"
+PLAYER_TOKEN = 'X'
+COMPUTER_TOKEN = 'O'
+EMPTY = ' '
+field = {'7': EMPTY, '8': EMPTY, '9': EMPTY,
+         '4': EMPTY, '5': EMPTY, '6': EMPTY,
+         '1': EMPTY, '2': EMPTY, '3': EMPTY}
 
 
-def create_table(dict):
-    print(dict[7] + " | " + dict[8] + " | " + dict[9])
-    print(f"---------")
-    print(dict[4] + " | " + dict[5] + " | " + dict[6])
-    print(f"---------")
-    print(dict[1] + " | " + dict[2] + " | " + dict[3])
+def playing_field(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
 
-def get_user_input():
+def play():
+    turn = PLAYER_TOKEN
+
     while True:
-        user_move = input("Please Select the Field: ")
-        if user_move in field_numbers:
-            return user_move
+        available = possible_moves(field)
+        playing_field(field)
+        if not available:
+            print(">>> Draw! <<<")
+            with open("../log.txt", "a") as log:
+                log.write(f"\n--> Draw!")
+            return False
+        if turn == PLAYER_TOKEN:
+            print("It's your turn! " + turn + ". Please choose a field position: ")
+        if turn == COMPUTER_TOKEN:
+            move = computer_move(field)
         else:
-            print("Please enter a Number from 1 to 9...")
-
-
-def computer_move(dict):
-    computer = True
-    while computer:
-        move = random.randint(1, 9)
-        if dict[move] == "":
-            return move
-
-
-def block_user(field):
-    computer = True
-    while computer:
-        player_1 = "X"
-        if field[1] == player_1 and field[2] == player_1:
-            if field[3] == "":
-                return 3
-        elif field[1] == player_1 and field[7] == player_1:
-            if field[4] == "":
-                return 4
-        elif field[1] == player_1 and field[4] == player_1:
-            if field[7] == "":
-                return 7
-        elif field[1] == player_1 and field[3] == player_1:
-            if field[2] == "":
-                return 2
-        elif field[1] == player_1 and field[5] == player_1:
-            if field[9] == "":
-                return 9
-        elif field[1] == player_1 and field[9] == player_1:
-            if field[5] == "":
-                return 5
-
-        elif field[2] == player_1 and field[1] == player_1:
-            if field[3] == "":
-                return 3
-        elif field[2] == player_1 and field[3] == player_1:
-            if field[2] == "":
-                return 1
-        elif field[2] == player_1 and field[8] == player_1:
-            if field[5] == "":
-                return 5
-        elif field[2] == player_1 and field[5] == player_1:
-            if field[8] == "":
-                return 8
-
-        elif field[3] == player_1 and field[1] == player_1:
-            if field[2] == "":
-                return 2
-        elif field[3] == player_1 and field[2] == player_1:
-            if field[1] == "":
-                return 1
-        elif field[3] == player_1 and field[9] == player_1:
-            if field[6] == "":
-                return 6
-        elif field[3] == player_1 and field[6] == player_1:
-            if field[9] == "":
-                return 9
-        elif field[3] == player_1 and field[5] == player_1:
-            if field[7] == "":
-                return 7
-        elif field[3] == player_1 and field[7] == player_1:
-            if field[5] == "":
-                return 5
-
-        elif field[4] == player_1 and field[1] == player_1:
-            if field[7] == "":
-                return 7
-        elif field[4] == player_1 and field[7] == player_1:
-            if field[1] == "":
-                return 1
-        elif field[4] == player_1 and field[6] == player_1:
-            if field[5] == "":
-                return 5
-        elif field[4] == player_1 and field[5] == player_1:
-            if field[6] == "":
-                return 6
-
-        elif field[5] == player_1 and field[1] == player_1:
-            if field[9] == "":
-                return 9
-        elif field[5] == player_1 and field[4] == player_1:
-            if field[6] == "":
-                return 6
-        elif field[5] == player_1 and field[7] == player_1:
-            if field[3] == "":
-                return 3
-        elif field[5] == player_1 and field[2] == player_1:
-            if field[8] == "":
-                return 8
-        elif field[5] == player_1 and field[8] == player_1:
-            if field[2] == "":
-                return 2
-        elif field[5] == player_1 and field[3] == player_1:
-            if field[7] == "":
-                return 7
-        elif field[5] == player_1 and field[6] == player_1:
-            if field[4] == "":
-                return 4
-        elif field[5] == player_1 and field[9] == player_1:
-            if field[1] == "":
-                return 1
-
-        elif field[6] == player_1 and field[3] == player_1:
-            return 9
-        elif field[6] == player_1 and field[9] == player_1:
-            return 3
-        elif field[6] == player_1 and field[5] == player_1:
-            return 4
-        elif field[6] == player_1 and field[4] == player_1:
-            return 5
-
-        elif field[7] == player_1 and field[8] == player_1:
-            return 9
-        elif field[7] == player_1 and field[9] == player_1:
-            return 8
-        elif field[7] == player_1 and field[5] == player_1:
-            return 3
-        elif field[7] == player_1 and field[3] == player_1:
-            return 5
-        elif field[7] == player_1 and field[4] == player_1:
-            return 1
-        elif field[7] == player_1 and field[1] == player_1:
-            return 4
-
-        elif field[8] == player_1 and field[7] == player_1:
-            return 9
-        elif field[8] == player_1 and field[9] == player_1:
-            return 7
-        elif field[8] == player_1 and field[5] == player_1:
-            return 2
-        elif field[8] == player_1 and field[2] == player_1:
-            return 5
-
-        elif field[9] == player_1 and field[7] == player_1:
-            return 8
-        elif field[9] == player_1 and field[8] == player_1:
-            return 7
-        elif field[9] == player_1 and field[6] == player_1:
-            return 3
-        elif field[9] == player_1 and field[3] == player_1:
-            return 6
-        elif field[9] == player_1 and field[5] == player_1:
-            return 1
-        elif field[9] == player_1 and field[1] == player_1:
-            return 5
+            move = input()
+            if input_check(move, turn):
+                continue
+        if field[move] == EMPTY:
+            field[move] = turn
+            print(f"--> Player {turn} has picked {move}...")
+            with open("../log.txt", "a") as log:
+                log.write(f"--> {turn} has picked {move}...\n")
         else:
-            if field[5] == "":
-                return 5
-            elif field[1] == "":
-                return 1
-            elif field[3] == "":
-                return 3
-            elif field[7] == "":
-                return 7
-            elif field[9] == "":
-                return 9
-            else:
-                move = random.randint(1, 9)
-                if field[move] == "":
-                    return move
+            print("This field is already occupied.\nPlease choose another field.")
+            continue
+        if check_win(field):
+            playing_field(field)
+            print(f">>>> {turn} has won! <<<<")
+            with open("../log.txt", "a") as log:
+                log.write(f"\n--> {turn} has won! \n-->Moves needed: {move}")
+            sys.exit(0)
+        if turn == PLAYER_TOKEN:
+            turn = COMPUTER_TOKEN
+        else:
+            turn = PLAYER_TOKEN
 
 
-def check_for_win(dict, moves):
-    player_1 = "X"
-    player_2 = "O"
-    if moves >= 10:
-        print("Tie! Nobody has won")
-    elif dict[7] == player_1 and dict[4] == player_1 and dict[1] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[8] == player_1 and dict[5] == player_1 and dict[2] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[9] == player_1 and dict[6] == player_1 and dict[3] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[9] == player_1 and dict[8] == player_1 and dict[7] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[6] == player_1 and dict[5] == player_1 and dict[4] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[1] == player_1 and dict[2] == player_1 and dict[3] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[1] == player_1 and dict[5] == player_1 and dict[9] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-    elif dict[7] == player_1 and dict[5] == player_1 and dict[3] == player_1:
-        print(f"{player_1} has won! Congrats")
-        return False
-
-    elif dict[7] == player_2 and dict[4] == player_2 and dict[1] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[8] == player_2 and dict[5] == player_2 and dict[2] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[9] == player_2 and dict[6] == player_2 and dict[3] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[9] == player_2 and dict[8] == player_2 and dict[7] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[6] == player_2 and dict[5] == player_2 and dict[4] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[1] == player_2 and dict[2] == player_2 and dict[3] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[1] == player_2 and dict[5] == player_2 and dict[9] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    elif dict[7] == player_2 and dict[5] == player_2 and dict[3] == player_2:
-        print(f"{player_2} has won! Congrats")
-        return False
-    else:
+def check_win(board):
+    if board['7'] == board['8'] == board['9'] != EMPTY:
         return True
+    if board['4'] == board['5'] == board['6'] != EMPTY:
+        return True
+    if board['1'] == board['2'] == board['3'] != EMPTY:
+        return True
+    if board['1'] == board['5'] == board['9'] != EMPTY:
+        return True
+    if board['3'] == board['5'] == board['7'] != EMPTY:
+        return True
+    if board['7'] == board['4'] == board['1'] != EMPTY:
+        return True
+    if board['8'] == board['5'] == board['2'] != EMPTY:
+        return True
+    if board['9'] == board['6'] == board['3'] != EMPTY:
+        return True
+    return False
 
 
-def write_log(player, move):
-    if player == 2:
-        with open("log.txt", "a") as log:
-            log.write(f"--> Computer has picked {move}... \n")
-    else:
-        with open("log.txt", "a") as log:
-            log.write(f"--> Player {player} has picked {move}... \n")
+def possible_moves(board):
+    return [k for k, v in board.items() if v == EMPTY]
 
 
-def create_header(move):
-    with open("log.txt", "a") as log:
-        log.write("\n\n >>> NEW-GAME <<< \n")
+def input_check(insert, turn):
+    if insert.lower() == 'end':
+        print(f"Game has been ended by Player {turn}")
+        with open("../log.txt", "a") as log:
+            log.write(f"\n--> Game has been ended by Player {turn}")
+        sys.exit(0)
+    elif insert.lower() == "clear":
+        clear_logs()
+    elif insert.lower() == "admin-login":
+        admin = input("Bitte geben sie das Passwort ein: ")
+        if admin == PASSWORD:
+            print("$ Welcome Admin...")
+    try:
+        int(insert)
+    except ValueError:
+        print("Please enter a number!")
+        return True
+    if int(insert) < 1 or int(insert) > 9:
+        print("Please enter a number from 1 to 9!")
+        return True
+    return False
+
+
+def computer_move(board):
+    copy_field = board.copy()
+    # Kontrolle ob Computer eine Chance zum Siegen hat.
+    for k, v in board.items():
+        if v == EMPTY:
+            copy_field[k] = COMPUTER_TOKEN
+            if check_win(copy_field):
+                move = k
+                return move
+            else:
+                copy_field[k] = EMPTY
+    # Bei keiner Chance zum Sieg, Rückgabe von zufällig generierter Zahl.
+    return computer_defend(board)
+
+
+def computer_defend(board):
+    copy_field = board.copy()
+    for k, v in board.items():
+        if v == EMPTY:
+            copy_field[k] = PLAYER_TOKEN
+            if check_win(copy_field):
+                move = k
+                return move
+            else:
+                copy_field[k] = EMPTY
+    return str(random.choice(possible_moves(board)))
 
 
 def clear_logs():
@@ -258,7 +137,7 @@ def clear_logs():
     while progress:
         confirm_deletion = input("Are you sure you want to delete the logs? no [n] yes [y]: ")
         if confirm_deletion.lower() == "y":
-            with open("log.txt", "w") as log:
+            with open("../log.txt", "w") as log:
                 pass
             print("The Logs have been deleted...")
             return
@@ -269,48 +148,7 @@ def clear_logs():
             print("Please enter either [n] for no or [y] for yes...")
 
 
-def log_moves(moves):
-    with open("log.txt", "a") as log:
-        log.write(f"\n--> Moves needed: {moves} \n")
-
-
-def game():
-    moves = 0
-    player = 1
-    table = {1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "", 9: ""}
-    if moves <= 0:
-        create_table(table)
-        create_header(moves)
-    while check_for_win(table, moves):
-        if player == 1:
-            user_move = get_user_input()
-            if user_move.lower() == "clear":
-                clear_logs(user_move)
-            else:
-                if table[int(user_move)] == "":
-                    if player == 1:
-                        table[int(user_move)] = "X"
-                        write_log(player, user_move)
-                        player = 2
-                        moves += 1
-        elif player == 2:
-            com_move = block_user(table)
-            table[com_move] = "O"
-            write_log(player, com_move)
-            player = 1
-            moves += 1
-            print(f"Computer picks {com_move}...")
-        else:
-            print("This Field has already been selected...")
-        create_table(table)
-    log_moves(moves)
-
-
 while True:
-    ask_for_start = input("Press [n] to start a new Game: ")
-    if ask_for_start.lower() == "n":
-        game()
-    elif ask_for_start.lower() == "clear":
-        clear_logs()
-    else:
-        print("Please press the [n]-Button to start the new Game...")
+    with open("../log.txt", "a") as log:
+        log.write("\n\n >>> NEW-GAME <<< \n\n")
+    play()
